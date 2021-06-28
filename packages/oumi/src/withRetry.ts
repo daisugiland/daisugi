7673560543c5061e12b79be862c23d18bc45bd40
Alexander Sviridov <sviridoff.mail@gmail.com>
2021-06-28 23:30:35 +0200

    added full jitter

--- packages/oumi/src/withRetry.ts
+++ packages/oumi/src/withRetry.ts
@@ -1,6 +1,7 @@
 import { waitFor } from './waitFor';
 
 const STARTING_DELAY_MS = 100;
+const MAX_DELAY_MS = 300;
 const TIME_MULTIPLIER = 2;
 const MAX_ATTEMPTS = 5;
 
@@ -12,14 +13,13 @@ async function fnWithRetry(asyncFn, args, attempt) {
   const result = asyncFn(...args);
 
   if (result.isFailure && attempt < MAX_ATTEMPTS) {
-    const delayMs =
-      STARTING_DELAY_MS * TIME_MULTIPLIER ** attempt;
-
-    // https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
-    const delayWithJitterMs = randomBetween(
-      delayMs / 2,
-      delayMs,
+    const delayMs = Math.min(
+      MAX_DELAY_MS,
+      STARTING_DELAY_MS * TIME_MULTIPLIER ** attempt,
     );
+
+    // Full jitter https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+    const delayWithJitterMs = randomBetween(0, delayMs);
 
     await waitFor(delayWithJitterMs);
 
