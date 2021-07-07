5174bd1a15cd0599057b1c85dda52e8b805d37b5
Alexander Sviridov <sviridoff.mail@gmail.com>
2021-07-07 23:42:22 +0200

    wip vasa

--- packages/vasa/src/__tests__/vasa.spec.ts
+++ packages/vasa/src/__tests__/vasa.spec.ts
@@ -1,15 +1,11 @@
 import { vasa } from '../vasa';
 
 describe('#vasa()', () => {
-  it('a', () => {
+  it('useClass', () => {
     const { container } = vasa();
 
     class A {
-      b;
-
-      constructor(b) {
-        this.b = b;
-      }
+      constructor(public b) {}
     }
 
     class B {
@@ -29,7 +25,109 @@ describe('#vasa()', () => {
     ]);
 
     const a = container.resolve('A');
+    const anotherA = container.resolve('A');
 
     expect(a.b.foo).toBe('foo');
+    expect(a).toBe(anotherA);
+  });
+
+  it('useValue', () => {
+    const { container } = vasa();
+
+    class A {
+      constructor(public a) {}
+    }
+
+    container.register([
+      {
+        token: 'A',
+        useClass: A,
+        params: ['B'],
+      },
+      {
+        token: 'B',
+        useValue: 'foo',
+      },
+    ]);
+
+    const a = container.resolve('A');
+
+    expect(a.a).toBe('foo');
+  });
+
+  it('scope', () => {
+    const { container } = vasa();
+
+    class A {
+      constructor() {}
+    }
+
+    container.register([
+      {
+        token: 'A',
+        useClass: A,
+        scope: 'Transient',
+      },
+    ]);
+
+    const a = container.resolve('A');
+    const anotherA = container.resolve('A');
+
+    expect(a).not.toBe(anotherA);
+  });
+
+  it('nested scope', () => {
+    const { container } = vasa();
+
+    class A {
+      constructor(public b) {}
+    }
+
+    class B {}
+
+    container.register([
+      {
+        token: 'A',
+        useClass: A,
+        params: ['B'],
+      },
+      {
+        token: 'B',
+        useClass: B,
+        scope: 'Transient',
+      },
+    ]);
+
+    const a = container.resolve('A');
+    const anotherA = container.resolve('A');
+
+    expect(a.b).toBe(anotherA.b);
+  });
+
+  it('useFactory', () => {
+    const { container } = vasa();
+
+    function useFactory(container) {
+      if (container.resolve('B') === 'foo') {
+        return 'yes';
+      }
+
+      return 'no';
+    }
+
+    container.register([
+      {
+        token: 'B',
+        useValue: 'foo',
+      },
+      {
+        token: 'A',
+        useFactory,
+      },
+    ]);
+
+    const a = container.resolve('A');
+
+    expect(a).toBe('yes');
   });
 });
--- packages/vasa/src/vasa.ts
+++ packages/vasa/src/vasa.ts
@@ -1,18 +1,69 @@
-interface VasaManifest {
+interface VasaManifestItem {
   token: string;
-  useClass: any;
+  useClass?: any;
+  useValue?: any;
+  useFactory?: any;
   params?: string[];
+  scope?: 'Transient' | 'Singleton';
+}
+
+interface PrivateManifestItem {
+  token: string;
+  useClass?: any;
+  useValue?: any;
+  useFactory?: any;
+  params?: string[];
+  instance?: any;
+  scope?: 'Transient' | 'Singleton';
 }
 
 export function vasa() {
-  const store = Object.create(null);
+  const tokenToManifest: Record<
+    string,
+    PrivateManifestItem
+  > = Object.create(null);
 
   return {
     container: {
-      resolve() {},
-      register(manifests: VasaManifest[]) {
-        manifests.forEach((manifest) => {
-          store[manifest.token] = 'a';
+      resolve(token) {
+        const manifestItem = tokenToManifest[token];
+
+        if (manifestItem.useValue) {
+          return manifestItem.useValue;
+        }
+
+        if (manifestItem.instance) {
+          return manifestItem.instance;
+        }
+
+        if (manifestItem.useFactory) {
+          return manifestItem.useFactory(this);
+        }
+
+        let paramsInstance = null;
+
+        if (manifestItem.params) {
+          paramsInstance = manifestItem.params.map(
+            (param) => this.resolve(param),
+          );
+        }
+
+        if (manifestItem.scope === 'Transient') {
+          return paramsInstance
+            ? new manifestItem.useClass(...paramsInstance)
+            : new manifestItem.useClass();
+        }
+
+        manifestItem.instance = paramsInstance
+          ? new manifestItem.useClass(...paramsInstance)
+          : new manifestItem.useClass();
+
+        return manifestItem.instance;
+      },
+      register(manifest: VasaManifestItem[]) {
+        manifest.forEach((manifestItem) => {
+          tokenToManifest[manifestItem.token] =
+            manifestItem;
         });
       },
     },
