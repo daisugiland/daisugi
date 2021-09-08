82f1c5e73bc4d5ea1990234d7ec7ceecec833b19
Alexander Sviridov <sviridoff.mail@gmail.com>
2021-09-08 22:55:12 +0200

    added lerna

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-yarn workspace @daisugi/kintsugi build
-yarn workspace @daisugi/daisugi build
-yarn workspace @daisugi/oza build
-yarn workspace @daisugi/kado build
--- bin/test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-yarn workspace @daisugi/kintsugi test
-yarn workspace @daisugi/daisugi test
-# yarn workspace @daisugi/oza test
-yarn workspace @daisugi/kado test
