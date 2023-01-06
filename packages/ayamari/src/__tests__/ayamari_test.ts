b2626f5bbf24dfc5532ba83f1875d07f0eb9267a
sviridoff <sviridoff.mail@gmail.com>
2023-01-07 00:25:43 +0100

    implemented ayamari

--- packages/ayamari/src/__tests__/ayamari_test.ts
+++ packages/ayamari/src/__tests__/ayamari_test.ts
@@ -3,21 +3,34 @@ import { test } from 'node:test';
 
 import { appErr } from '../ayamari.js';
 
-function err1() {
-  return appErr.NotFound('Not found 1');
+function err1(param: string) {
+  try {
+    return new URL(param);
+  } catch (err) {
+    return appErr.NotFound('Not found 1', {
+      cause: err as Error,
+      args: arguments,
+    });
+  }
 }
 
-function err2() {
+function err2(_: string) {
   return appErr.NotFound('Not found 2', {
-    cause: err1().getError(),
+    // @ts-ignore
+    cause: err1('bar').getError(),
+    args: arguments,
+    metadata: {
+      foo: 'bar',
+    },
   });
 }
 
 test('Ayamari', async (t) => {
   await t.test('should work', async () => {
-    const errRes = err2();
+    const errRes = err2('foo');
     const err = errRes.getError();
     console.log(err.prettyStack());
+    console.log(err);
     assert.equal(err.prettyStack(), '');
   });
 });
