5faea9d93b585cad5bff1f0c7a92a058723ac649
sviridoff <sviridoff.mail@gmail.com>
2023-09-09 22:35:47 +0200

    feat(yomeru): project skaffold

---
+++ packages/yomeru/src/__tests__/yomeru_test.ts
@@ -0,0 +1,8 @@
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+describe('Result', () => {
+  it('should return expected value', () => {
+    assert.equal(true, true);
+  });
+});
