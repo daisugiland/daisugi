04fb9ba26091950de4065e53a2fc34ec2d3d6613
sviridoff <sviridoff.mail@gmail.com>
2023-09-09 22:37:22 +0200

    type

--- packages/yomeru/src/__tests__/yomeru_test.ts
+++ packages/yomeru/src/__tests__/yomeru_test.ts
@@ -1,7 +1,7 @@
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 
-describe('Result', () => {
+describe('yomeru', () => {
   it('should return expected value', () => {
     assert.equal(true, true);
   });
